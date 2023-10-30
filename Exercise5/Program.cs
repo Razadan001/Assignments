f004d86762d438d5bc062e3db7f0fd7596790bae
Ahmadraza Danmole <razadanmole@gmail.com>
2023-10-31 00:34:43 +0100

    Task 5

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -1,5 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
 //Question 1
 // static void Question1()
 // {
@@ -17,31 +18,74 @@ Console.WriteLine("Hello, World!");
 
 
 //Question2
-static void Question2()
-{
-    Console.Write("Enter the first real number: ");
-    double number1 = double.Parse(Console.ReadLine()!);
+// 
 
-    Console.Write("Enter the second real number: ");
-    double number2 = double.Parse(Console.ReadLine()!);
+//Question 3
+// static void Question3()
+// {
+//     int num1, num2, num3;
+//     Random random = new Random();
+//     num1 = random.Next(0,5);
+//     num2 = random.Next(0, 5);
+//     num3 = random.Next(0, 5);
+//     if(num1 > num2 && num1 > num3)
+//     {
+//         Console.WriteLine($"The biggest number is: {num1} out of {num1}, {num2}, {num3}");
+//     }
+//     else if(num2 > num1 && num2 > num3)
+//     {
+//         Console.WriteLine($"The biggest number is: {num2} out of {num1}, {num2}, {num3}");
+//     }
+//     else if(num3 > num1 && num3 > num2)
+//     {
+//         Console.WriteLine($"The biggest number is: {num3} out of {num1}, {num2}, {num3}");
+//     }
+//     else
+//     {
+//         Console.Write("Unexcpected number");
+//     }
+// }
+// Question3();
 
-    Console.Write("Enter the third real number: ");
-    double number3 = double.Parse(Console.ReadLine()!);
+//Question 4
+// static void Question4()
+// {
+//     int num1, num2, num3;
+//     Console.Write("Input number: ");
+//     num1 = int.Parse(Console.ReadLine()!);
+//     Console.Write("Input number: ");
+//     num2 = int.Parse(Console.ReadLine()!);
+//     Console.Write("Input number: ");
+//     num3 = int.Parse(Console.ReadLine()!);
+//     if(num1 > num2 && num1 > num3 && num2 > num3)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num1}\n{num2}\n{num3}");
+//     }
+//     else if(num1 > num2 && num1 > num3 && num3 > num2)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num1}\n{num3}\n{num2}");
+//     }
+//     else if(num2 > num1 && num2 > num3 &&  num1 > num3)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num2}\n{num1}\n{num3}");
+//     }
+//     else if(num2 > num1 && num2 > num3 &&  num3 > num1)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num2}\n{num3}\n{num1}");
+//     }
+//     else if(num3 > num1 && num3 > num2 && num2 > num1)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num3}\n{num2}\n{num1}");
+//     }
+//     else if(num3 > num1 && num3 > num2 && num1 > num2)
+//     {
+//         Console.WriteLine($"Arranged in Descending order.\n{num3}\n{num1}\n{num2}");
+//     }
+//     else
+//     {
+//         Console.Write("Unexcpected number");
+//     }
+// }
+// Question4();
 
-    char Symbol = '+';
-
-    if (number1 < 0)
-    {
-        Symbol = (number2 < 0) ? '+' : '-';
-    }
-    else if (number2 < 0)
-    {
-        Symbol = (number3 < 0) ? '+' : '-';
-    }
-    else if (number3 < 0)
-    {
-        Symbol = '-';
-    }
-
-    Console.WriteLine("The sign of the product is: " + Symbol);
-}
+// Question 5;
